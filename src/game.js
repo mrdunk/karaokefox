36f174359ca25b9e231a2fb4b926cd73748a340b
duncan law <mrdunk@gmail.com>
2018-11-18 12:07:41 +0000

    Move fox character to it's own object.

--- src/game.js
+++ src/game.js
@@ -1,43 +1,104 @@
 ///<reference path="3rdParty/babylon.d.ts" />
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var SCENEPATH = "scenes/";
 var FOX = "fox.babylon";
 //let FOX = "fox.stl";
 // let FOX = "skull.babylon";
+var SCALE = 100;
+var Character = /** @class */ (function () {
+    function Character(scene, shaddows, filename, onLoaded) {
+        console.log("Creating Character from " + filename);
+        this._scene = scene;
+        this._shaddows = shaddows;
+        this._onLoaded = onLoaded;
+        this._bones = {};
+        this._lookAtNeck = new BABYLON.Vector3(0, 0, 0);
+        this.ahead = new BABYLON.Vector3(0, 0, 0);
+        BABYLON.SceneLoader.ImportMesh("", SCENEPATH, filename, this._scene, this.onSceneLoad.bind(this));
+    }
+    Character.prototype.onSceneLoad = function (meshes, particleSystems, skeletons) {
+        try {
+            console.assert(meshes.length === 1);
+            console.assert(particleSystems.length === 0);
+            console.assert(skeletons.length === 1);
+            this._mesh = meshes[0];
+            this._skeleton = skeletons[0];
+            // this._mesh.isVisible = false;
+            this.position = this._mesh.position;
+            this.rotation = this._mesh.rotation;
+            this._mesh.scaling = new BABYLON.Vector3(SCALE, SCALE, SCALE);
+            if (this._shaddows) {
+                this._shaddows.getShadowMap().renderList.push(this._mesh);
+            }
+            var skeletonViewer = new BABYLON.Debug.SkeletonViewer(this._skeleton, this._mesh, this._scene);
+            skeletonViewer.isEnabled = true; // Enable it
+            skeletonViewer.color = BABYLON.Color3.Red(); // Change default color from white to red
+            for (var index = 0; index < this._skeleton.bones.length; index++) {
+                var bone = skeletons[0].bones[index];
+                // console.log(bone.uniqueId, bone.id);
+                switch (bone.id) {
+                    case "spine.head":
+                        this._bones.head = bone;
+                        break;
+                    case "spine.neck":
+                        this._bones["neck"] = bone;
+                        break;
+                }
+            }
+            this._scene.registerBeforeRender(function () {
+                if (!this.position.equals(this._mesh.position)) {
+                    this._mesh.position.x = this.position.x;
+                    this._mesh.position.y = this.position.y;
+                    this._mesh.position.z = this.position.z;
+                }
+                if (!this.rotation.equals(this._mesh.rotation)) {
+                    this._mesh.rotation.x = this.rotation.x;
+                    this._mesh.rotation.y = this.rotation.y;
+                    this._mesh.rotation.z = this.rotation.z;
+                }
+            }.bind(this));
+            //this._scene.beginAnimation(this._skeleton, 1, 30, true);
+            if (this._onLoaded) {
+                this._onLoaded();
+            }
+            this._lookCtrlHead = new BABYLON.BoneLookController(this._mesh, this._bones.head, this._lookAt, { adjustPitch: Math.PI * .5 });
+            /*this._lookCtrlNeck = new BABYLON.BoneLookController(
+              this._mesh,
+              this._bones.neck,
+              this._lookAtNeck,
+              {adjustPitch:Math.PI*.5}
+            );*/
+        }
+        catch (error) {
+            // Prevent error messages in this section getting swallowed by Babylon.
+            console.error(error);
+        }
+    };
+    Character.prototype.lookAt = function (target) {
+        this._lookAt = target;
+        //var localPos1 = new BABYLON.Vector3(0, 10, 20);;
+        this._scene.registerBeforeRender(function () {
+            // TODO Can we optimise here? We probably copy more often than required.
+            var neck = this._bones.neck;
+            //let neckPosWorld = neck.getPosition(BABYLON.Space.WORLD, this._mesh);
+            //let neckPosWorld = neck.getAbsolutePosition(this._mesh);
+            //let targetDistance = BABYLON.Vector3.Distance(neckPosWorld, this._lookAt);
+            //let neckPosLocal = neck.getLocalPositionFromAbsolute(neckPosWorld, this.mesh);
+            var neckPosLocal = new BABYLON.Vector3(0, 44, 23);
+            //this.ahead = neck.getAbsolutePositionFromLocal(new BABYLON.Vector3(neckPosLocal.x, neckPosLocal.y - targetDistance, neckPosLocal.z), this.mesh);
+            //this.ahead = neck.getAbsolutePositionFromLocal(new BABYLON.Vector3(neckPosLocal.x, 100, neckPosLocal.z), this.mesh);
+            neck.getAbsolutePositionFromLocalToRef(neckPosLocal, this._mesh, this.ahead);
+            //this.ahead = this._skeleton.bones[0].getAbsolutePositionFromLocal(localPos1, this._mesh);
+            //console.log(neckPosWorld, ahead, targetDistance);
+            console.log(this.ahead);
+            //this._lookAtNeck.x = (this.ahead.x + this._lookAt.x) / 2;
+            //this._lookAtNeck.y = (this.ahead.y + this._lookAt.y) / 2;
+            //this._lookAtNeck.z = (this.ahead.z + this._lookAt.z) / 2;
+            //this._lookCtrlNeck.update();
+            this._lookCtrlHead.update();
+        }.bind(this));
+    };
+    return Character;
+}());
 var Game = /** @class */ (function () {
     function Game(canvasElement) {
         // Create canvas and engine.
@@ -45,6 +106,7 @@ var Game = /** @class */ (function () {
         this._engine = new BABYLON.Engine(this._canvas, true);
     }
     Game.prototype.createScene = function () {
+        var _this = this;
         this._scene = new BABYLON.Scene(this._engine);
         this._scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
         this._light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), this._scene);
@@ -64,52 +126,35 @@ var Game = /** @class */ (function () {
         // Shadows
         var shadowGenerator = new BABYLON.ShadowGenerator(1024, this._light);
         // Meshes
-        var that = this;
-        BABYLON.SceneLoader.ImportMesh("", SCENEPATH, FOX, this._scene, function (meshes, particleSystems, skeletons) {
-            var _this = this;
-            var fox = meshes[0];
-            fox.scaling = new BABYLON.Vector3(100, 100, 100);
+        var debugBase = BABYLON.MeshBuilder.CreateBox("debugBase", { height: 5, width: 50, depth: 100 }, this._scene);
+        // Moving ball for the fox to watch.
+        var targetHead = BABYLON.MeshBuilder.CreateSphere("targetHead", {}, this._scene);
+        var targetNeck = BABYLON.MeshBuilder.CreateSphere("targetNeck", { diameterX: 10, diameterY: 10, diameterZ: 10 }, this._scene);
+        var fox = new Character(this._scene, shadowGenerator, FOX, function () {
+            console.log("fox loaded");
+            _this._camera.target = fox.position;
+            fox.lookAt(targetHead.position);
             fox.rotation.y = Math.PI;
-            for (var index = 0; index < meshes.length; index++) {
-                shadowGenerator.getShadowMap().renderList.push(meshes[index]);
-            }
-            that._camera.target = fox.position;
-            var interval = setInterval(function () { return __awaiter(_this, void 0, void 0, function () {
-                var anim;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            anim = that._scene.beginAnimation(skeletons[0], 1, 30, false, 0.5);
-                            return [4 /*yield*/, anim.waitAsync()];
-                        case 1:
-                            _a.sent();
-                            anim = that._scene.beginAnimation(skeletons[0], 30, 1, false, 1);
-                            return [4 /*yield*/, anim.waitAsync()];
-                        case 2:
-                            _a.sent();
-                            anim = that._scene.beginAnimation(skeletons[0], 32, 61, false, 0.5);
-                            return [4 /*yield*/, anim.waitAsync()];
-                        case 3:
-                            _a.sent();
-                            anim = that._scene.beginAnimation(skeletons[0], 61, 32, false, 1.5);
-                            /*let anim1 = that._scene.beginWeightedAnimation(skeletons[0], 1, 30, 0.5, false, 0.5);
-                            let anim2 = that._scene.beginWeightedAnimation(skeletons[0], 32, 61, 0.5, false, 0.5);
-                            await anim1.waitAsync();
-                            await anim2.waitAsync();
-                            that._scene.beginWeightedAnimation(skeletons[0], 61, 32, 0.5, false, 0.5);
-                            that._scene.beginWeightedAnimation(skeletons[0], 30, 1, 0.5, false, 0.5);*/
-                            console.log("done");
-                            return [2 /*return*/];
-                    }
-                });
-            }); }, 3000);
         });
-        /*BABYLON.SceneLoader.ImportMesh("", SCENEPATH, FOX, this._scene, function (newMeshes) {
-          // Set the target of the camera to the first imported mesh
-          newMeshes[0].scaling = new BABYLON.Vector3(100, 100, 100);
-          newMeshes[0].material.backFaceCulling = true;
-          this._camera.target = newMeshes[0];
-        });*/
+        var t1 = 0;
+        var t2 = 0;
+        var t3 = 1;
+        var interval = setInterval(function () {
+            t1 += .02;
+            t2 += .03;
+            t3 += .001;
+            targetHead.position.x = 20 * Math.sin(t1);
+            targetHead.position.y = 44 + 20 * Math.sin(t2);
+            targetHead.position.z = 50;
+            if (fox.ahead) {
+                targetNeck.position.x = fox.ahead.x;
+                targetNeck.position.y = fox.ahead.y;
+                targetNeck.position.z = fox.ahead.z;
+            }
+            if (fox.rotation) {
+                fox.rotation.y = Math.PI * t3;
+            }
+        }, 50);
     };
     Game.prototype.doRender = function () {
         var _this = this;
--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,143 @@ let SCENEPATH = "scenes/";
 let FOX = "fox.babylon";
 //let FOX = "fox.stl";
 // let FOX = "skull.babylon";
+let SCALE = 100;
+
+class Character {
+  private _scene: BABYLON.Scene;
+  private _shaddows: BABYLON.ShadowGenerator;
+  private _mesh: BABYLON.Mesh;
+  private _skeleton: BABYLON.Skeleton;
+  private _bones: {[id: string] : BABYLON.Bone};
+  private _onLoaded: () => void;
+  private _lookAt: BABYLON.Vector3;
+  private _lookAtNeck: BABYLON.Vector3;
+  private _lookCtrlHead: BABYLON.BoneLookController;
+  private _lookCtrlNeck: BABYLON.BoneLookController;
+
+  position: BABYLON.Vector3;
+  rotation: BABYLON.Vector3;
+
+  // debug
+  ahead: BABYLON.Vector3;
+
+  constructor(scene: BABYLON.Scene,
+              shaddows: BABYLON.ShadowGenerator,
+              filename: string,
+              onLoaded?: () => void) {
+    console.log("Creating Character from " + filename);
+    this._scene = scene;
+    this._shaddows = shaddows;
+    this._onLoaded = onLoaded;
+    this._bones = {};
+    this._lookAtNeck = new BABYLON.Vector3(0, 0, 0);
+    this.ahead = new BABYLON.Vector3(0, 0, 0);
+    BABYLON.SceneLoader.ImportMesh("", SCENEPATH, filename, this._scene, this.onSceneLoad.bind(this));
+  }
+
+  onSceneLoad(meshes: BABYLON.Mesh[], particleSystems: [], skeletons: BABYLON.Skeleton[]) : void {
+    try{
+      console.assert(meshes.length === 1);
+      console.assert(particleSystems.length === 0);
+      console.assert(skeletons.length === 1);
+
+      this._mesh = meshes[0];
+      this._skeleton = skeletons[0];
+
+      // this._mesh.isVisible = false;
+
+      this.position = this._mesh.position;
+      this.rotation = this._mesh.rotation;
+      this._mesh.scaling = new BABYLON.Vector3(SCALE, SCALE, SCALE);
+
+      if(this._shaddows) {
+        this._shaddows.getShadowMap().renderList.push(this._mesh);
+      }
+
+      let skeletonViewer = new BABYLON.Debug.SkeletonViewer(this._skeleton, this._mesh, this._scene);
+      skeletonViewer.isEnabled = true; // Enable it
+      skeletonViewer.color = BABYLON.Color3.Red(); // Change default color from white to red
+
+      for(let index = 0; index < this._skeleton.bones.length; index++) {
+        let bone = skeletons[0].bones[index];
+        // console.log(bone.uniqueId, bone.id);
+        switch(bone.id) {
+          case "spine.head":
+            this._bones.head = bone;
+            break;
+          case "spine.neck":
+            this._bones["neck"] = bone;
+            break;
+        }
+      }
+
+      this._scene.registerBeforeRender(function () {
+        if(! this.position.equals(this._mesh.position)) {
+          this._mesh.position.x = this.position.x;
+          this._mesh.position.y = this.position.y;
+          this._mesh.position.z = this.position.z;
+        }
+        if(! this.rotation.equals(this._mesh.rotation)) {
+          this._mesh.rotation.x = this.rotation.x;
+          this._mesh.rotation.y = this.rotation.y;
+          this._mesh.rotation.z = this.rotation.z;
+        }
+      }.bind(this));
+
+      //this._scene.beginAnimation(this._skeleton, 1, 30, true);
+
+      if(this._onLoaded) {
+        this._onLoaded();
+      }
+
+      this._lookCtrlHead = new BABYLON.BoneLookController(
+        this._mesh,
+        this._bones.head,
+        this._lookAt,
+        {adjustPitch:Math.PI*.5}
+      );
+      /*this._lookCtrlNeck = new BABYLON.BoneLookController(
+        this._mesh,
+        this._bones.neck,
+        this._lookAtNeck,
+        {adjustPitch:Math.PI*.5}
+      );*/
+    } catch(error) {
+      // Prevent error messages in this section getting swallowed by Babylon.
+      console.error(error);
+    }
+  }
+
+  lookAt(target: BABYLON.Vector3) : void {
+    this._lookAt = target;
+    
+    //var localPos1 = new BABYLON.Vector3(0, 10, 20);;
+
+    this._scene.registerBeforeRender(function () {
+      // TODO Can we optimise here? We probably copy more often than required.
+      let neck = this._bones.neck;
+
+      //let neckPosWorld = neck.getPosition(BABYLON.Space.WORLD, this._mesh);
+      //let neckPosWorld = neck.getAbsolutePosition(this._mesh);
+      //let targetDistance = BABYLON.Vector3.Distance(neckPosWorld, this._lookAt);
+      //let neckPosLocal = neck.getLocalPositionFromAbsolute(neckPosWorld, this.mesh);
+      let neckPosLocal = new BABYLON.Vector3(0, 44, 23);
+      //this.ahead = neck.getAbsolutePositionFromLocal(new BABYLON.Vector3(neckPosLocal.x, neckPosLocal.y - targetDistance, neckPosLocal.z), this.mesh);
+      //this.ahead = neck.getAbsolutePositionFromLocal(new BABYLON.Vector3(neckPosLocal.x, 100, neckPosLocal.z), this.mesh);
+      neck.getAbsolutePositionFromLocalToRef(neckPosLocal, this._mesh, this.ahead);
+      //this.ahead = this._skeleton.bones[0].getAbsolutePositionFromLocal(localPos1, this._mesh);
+      //console.log(neckPosWorld, ahead, targetDistance);
+      console.log(this.ahead);
+
+      //this._lookAtNeck.x = (this.ahead.x + this._lookAt.x) / 2;
+      //this._lookAtNeck.y = (this.ahead.y + this._lookAt.y) / 2;
+      //this._lookAtNeck.z = (this.ahead.z + this._lookAt.z) / 2;
+      //this._lookCtrlNeck.update();
+
+      this._lookCtrlHead.update();
+    }.bind(this));
+  }
+}
 
 class Game {
   private _canvas: HTMLCanvasElement;
@@ -43,44 +180,40 @@ class Game {
     let shadowGenerator = new BABYLON.ShadowGenerator(1024, this._light);
 
     // Meshes
-    let that = this;
-    BABYLON.SceneLoader.ImportMesh("", SCENEPATH, FOX, this._scene, function(meshes, particleSystems, skeletons) {
-      var fox = meshes[0];
-      fox.scaling = new BABYLON.Vector3(100, 100, 100);
+    let debugBase = BABYLON.MeshBuilder.CreateBox("debugBase", {height: 5, width: 50, depth: 100}, this._scene);
+    // Moving ball for the fox to watch.
+    let targetHead = BABYLON.MeshBuilder.CreateSphere("targetHead", {}, this._scene);
+    let targetNeck = BABYLON.MeshBuilder.CreateSphere("targetNeck", {diameterX: 10, diameterY: 10, diameterZ: 10}, this._scene);
+    
+    let fox = new Character(this._scene, shadowGenerator, FOX, () => {
+      console.log("fox loaded");
+      this._camera.target = fox.position;
+      fox.lookAt(targetHead.position);
       fox.rotation.y = Math.PI;
-
-      for (var index = 0; index < meshes.length; index++) {
-        shadowGenerator.getShadowMap().renderList.push(meshes[index]);
-      }
-
-      that._camera.target = fox.position;
-
-      let interval = setInterval(async () => {
-        let anim = that._scene.beginAnimation(skeletons[0], 1, 30, false, 0.5);
-        await anim.waitAsync();
-        anim = that._scene.beginAnimation(skeletons[0], 30, 1, false, 1);
-        await anim.waitAsync();
-        anim = that._scene.beginAnimation(skeletons[0], 32, 61, false, 0.5);
-        await anim.waitAsync();
-        anim = that._scene.beginAnimation(skeletons[0], 61, 32, false, 1.5);
-
-        /*let anim1 = that._scene.beginWeightedAnimation(skeletons[0], 1, 30, 0.5, false, 0.5);
-        let anim2 = that._scene.beginWeightedAnimation(skeletons[0], 32, 61, 0.5, false, 0.5);
-        await anim1.waitAsync();
-        await anim2.waitAsync();
-        that._scene.beginWeightedAnimation(skeletons[0], 61, 32, 0.5, false, 0.5);
-        that._scene.beginWeightedAnimation(skeletons[0], 30, 1, 0.5, false, 0.5);*/
-
-        console.log("done");
-      }, 3000);
     });
 
-    /*BABYLON.SceneLoader.ImportMesh("", SCENEPATH, FOX, this._scene, function (newMeshes) {
-      // Set the target of the camera to the first imported mesh
-      newMeshes[0].scaling = new BABYLON.Vector3(100, 100, 100);
-      newMeshes[0].material.backFaceCulling = true;
-      this._camera.target = newMeshes[0];
-    });*/
+    let t1 = 0;
+    let t2 = 0;
+    let t3 = 1;
+    let interval = setInterval( () => {
+      t1 += .02;
+      t2 += .03;
+      t3 += .001;
+
+      targetHead.position.x = 20 * Math.sin(t1);
+      targetHead.position.y = 44 + 20 * Math.sin(t2);
+      targetHead.position.z = 50;
+
+      if(fox.ahead) {
+        targetNeck.position.x = fox.ahead.x;
+        targetNeck.position.y = fox.ahead.y;
+        targetNeck.position.z = fox.ahead.z;
+      }
+
+      if(fox.rotation) {
+        fox.rotation.y = Math.PI * t3;
+      }
+    }, 50);
   }
 
   doRender() : void {
